5592753066487ebf3d3623fb654b5a9bed84f398
Damjan Dimitrov <damjandimitrov1@gmail.com>
2025-02-20 11:05:33 +0100

    Update template to include types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { Message } from "types";
 import { handlePayload } from "./handler";
 
 declare const _STD_: any;
@@ -8,24 +9,32 @@ _STD_.ws.open(
     "wss://websocket-proxy-2.prod.gke.acurast.com/",
   ],
   () => {
-    _STD_.ws.registerPayloadHandler(async ({ sender, recipient, payload }) => {
-      try {
-        const decoded = Buffer.from(payload, "hex").toString("utf8");
-        const parsed = JSON.parse(decoded);
+    _STD_.ws.registerPayloadHandler(
+      async ({ sender, recipient, payload }: Message) => {
+        try {
+          const decodedPayload = Buffer.from(payload, "hex").toString("utf8");
 
-        const response = await handlePayload(parsed, _STD_.env);
+          const response = await handlePayload(
+            {
+              payload: JSON.parse(decodedPayload),
+              recipient,
+              sender,
+            },
+            _STD_.env
+          );
 
-        _STD_.ws.send(
-          sender,
-          Buffer.from(JSON.stringify(response), "utf8").toString("hex")
-        );
-      } catch (e) {
-        _STD_.ws.send(
-          sender,
-          Buffer.from(`Error: ${e}`, "utf8").toString("hex")
-        );
+          _STD_.ws.send(
+            sender,
+            Buffer.from(JSON.stringify(response), "utf8").toString("hex")
+          );
+        } catch (e) {
+          _STD_.ws.send(
+            sender,
+            Buffer.from(`Error: ${e}`, "utf8").toString("hex")
+          );
+        }
       }
-    });
+    );
   },
   (err: any) => console.error(`open: error ${err}`)
 );
